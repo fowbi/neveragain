130f60e3a6d9444d5399477d5c1741e42dd3d5d5
Tobi Beernaert <tobi@magier.be>
2025-03-07 14:20:40 +0100

    Add color columns on 80 and 120 characters

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+
+vim.opt.colorcolumn = "80,120"
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#550000" })
