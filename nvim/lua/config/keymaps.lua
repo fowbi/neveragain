be0cd49ea8b6bfdab95512de7228af95f34f12f9
Tobi Beernaert <tobi@magier.be>
2025-09-01 11:33:57 +0200

    Add keymap to nvim for copying the current file location

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,4 +2,28 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+local keymap = vim.keymap.set
+
+keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Copy filepath of current file
+keymap("n", "<leader>yp", function()
+  local choices = {
+    { label = "Relative to CWD", value = vim.fn.expand("%:.") },
+    { label = "Absolute path", value = vim.fn.expand("%:p") },
+    { label = "Relative path", value = vim.fn.expand("%") },
+    { label = "Filename only", value = vim.fn.expand("%:t") },
+  }
+
+  vim.ui.select(choices, {
+    prompt = "Copy file path:",
+    format_item = function(item)
+      return item.label .. " → " .. item.value
+    end,
+  }, function(choice)
+    if choice then
+      vim.fn.setreg("+", choice.value)
+      vim.notify("Copied " .. choice.label .. ": " .. choice.value)
+    end
+  end)
+end, { desc = "Yank file path (choose type)" })
