334140def737420b99432a4c454aed8d6d2c827d
Tobi Beernaert <tobi@magier.be>
2025-10-23 11:03:44 +0200

    Add sketchybar config

---
+++ sketchybar/.config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+	text = "IosevkaTerm Nerd Font", -- Used for text
+	numbers = "IosevkaTerm Nerd Font", -- Used for numbers
+
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Regular",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Heavy",
+		["Black"] = "Black",
+	},
+}
---
+++ sketchybar/.config/sketchybar/settings.lua
@@ -0,0 +1,17 @@
+return {
+	paddings = 3,
+	group_paddings = 5,
+
+	icons = "NerdFont", -- alternatively available: NerdFont
+	font = {
+		text = "IosevkaTerm Nerd Font", -- Used for text
+		numbers = "IosevkaTerm Nerd Font", -- Used for numbers
+		style_map = {
+			["Regular"] = "Regular",
+			["Semibold"] = "Medium",
+			["Bold"] = "SemiBold",
+			["Heavy"] = "Bold",
+			["Black"] = "ExtraBold",
+		},
+	},
+}
