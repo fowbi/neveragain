ba9d1ee3d4904a9b5294479a96ea220142db0f23
Tobi Beernaert <tobi@magier.be>
2026-04-17 10:25:01 +0200

    fix colorscheme in nvim for >= 0.12

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,4 +1,18 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte", -- latte, frappe, macchiato, mocha
+      transparent_background = false,
+    },
+  },
   { "nvim-tree/nvim-web-devicons", opts = {} },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
 }
